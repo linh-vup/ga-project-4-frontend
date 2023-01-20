d8058d15cc425ab5a95e894607365900c8cf33b3
linhvu <linh.vup@gmail.com>
2023-01-20 12:24:50 +0000

    add navbar component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Login from './components/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { useNavigate, Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexgrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar variant='dense' className='Navbar'>
+          <Link to='/'>
+            <Typography
+              variant='h6'
+              color='inherit'
+              component='div'
+              sx={{ mr: 2, color: 'black' }}
+            >
+              Home
+            </Typography>
+          </Link>
+          <Link to='/login'>
+            <Typography
+              variant='h6'
+              color='inherit'
+              component='div'
+              sx={{ mr: 2, color: 'black' }}
+            >
+              Login
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
