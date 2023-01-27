30753282ed3e15d8a48469aed57e8282b4808668
linhvu <linh.vup@gmail.com>
2023-01-27 10:46:43 +0000

    remove unused component and update social share button comp

--- src/components/common/SocialShareButtons.js
+++ src/components/common/SocialShareButtons.js
@@ -2,11 +2,11 @@ import { TwitterShareButton, TwitterIcon } from 'react-share';
 
 export default function SocialSharebuttons() {
   const tweetText =
-    'I ate a full rainbow today! Check out Eat a Rainbow to log your colorful food';
+    'I ate a full rainbow today! Check out A Rainbow a Day to log your colorful food today';
 
   return (
     <TwitterShareButton
-      url={'https://www.example.com'}
+      url={'https://a-rainbow-a-day.netlify.app/'}
       title={tweetText}
       hashtag='#eattherainbow#healthyfood'
     >
